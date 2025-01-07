51ba4c0908f3ead0c99f60521dc461d9bc2bfd5f
Ronja Norlén <rono2300@student.miun.se>
2025-01-07 15:54:34 +0100

    installerat hapi, cookie, jwt, joi, skapat grund för user-routes, kataloger för controllers och models, skapat start-fil

---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi'); // Använd hapi
+const mongoose = require("mongoose"); // Inkludera mongoose
+require("dotenv").config(); // Använd dotenv för miljövariabler 
+// const auth = require('./auth'); // Importera autentiserings-fil
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: process.env.PORT || 5000,
+        host: process.env.HOST || 'localhost',
+        routes: {
+            cors: {
+                origin: ["*"], // Tillåt alla CORS-anrop
+                credentials: true,
+                maxAge: 86400,
+                headers: ["Accept", "Content-Type", "Access-Control-Allow-Origin"]
+            }
+        },
+    });
+
+    // Anslut till mongoDB
+    mongoose.connect(process.env.DATABASE).then(() => {
+        console.log("Ansluten till MongoDB");
+    }).catch((error) => {
+        console.error("Något gick fel vid anslutning till databasen: " + error); 
+    });
+
+    // Registrera autentisering 
+  //  await auth.register(server);
+
+    // Registrera routes 
+   // require('./routes/book.route')(server);
+    require('./routes/user.routes')(server);
+
+    // Starta servern 
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+// Hantering av ev fel 
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
---
+++ routes/user.routes.js
@@ -0,0 +1,67 @@
+const userController = require('../controllers/user.controller'); // Importera controller för användare 
+const Joi = require('joi'); // Inkludera Joi 
+
+module.exports = (server) => {
+    server.route({
+        // Hämta alla användare 
+        method: 'GET',
+        path: '/users',
+        handler: userController.getAllUsers,
+        options: {
+            auth: false
+        }
+    });
+    // Hämta användare per ID 
+    server.route({
+        method: 'GET',
+        path: '/users/{id}',
+        handler: userController.getUserById
+    });
+
+    // Skapa användare 
+    server.route({
+        method: 'POST',
+        path: '/users',
+        handler: userController.createUser,
+        options: {
+            auth: false
+        }
+    });
+
+    // Ta bort användare 
+    server.route({
+        method: 'DELETE',
+        path: '/users/{id}',
+        handler: userController.deleteUser
+    });
+
+    // Logga in användare 
+    server.route({
+        method: 'POST',
+        path: '/users/login',
+        handler: userController.loginUser,
+        options: {
+            auth: false,
+            validate: {
+                payload: Joi.object({
+                    email: Joi.string().min(1),
+                    password: Joi.string().min(1)
+                }),
+                failAction: (request, h, err) => {
+                    throw err;
+                }
+            }
+        }
+    });
+
+    // Logga ut användare 
+    server.route({
+        method: 'GET',
+        path: '/users/logout',
+        handler: userController.logoutUser,
+        options: {
+            auth: false
+        }
+    });
+
+}
